dff5a84697f81451b9b9792a052e4097ac510ac7
Rajiuddin <skrajiuddin18@gmail.com>
2025-06-05 18:17:20 +0530

    Header Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Logo from '../assets/logo.png'
+import './Header.css'
 
 export const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header>
+            <nav className="navbar bg-body-tertiary">
+                <div className="nav-container">
+                    <a className="logo" href="/">
+                        <img src={Logo} alt="Logo" width="30" height="24" />
+                    </a>
+                    <div className="nav-links-wrapper">
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) => isActive ? "active nav-links" : "nav-links"}>
+                            Home
+                        </NavLink>
+                        <NavLink
+                            to="/cart"
+                            className={({ isActive }) => isActive ? "active nav-links" : "nav-links"}>
+                            Cart
+                        </NavLink>
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+
+    )
 }
