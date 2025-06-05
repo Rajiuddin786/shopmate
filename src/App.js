22da1f279a1cc8534142013e9fcca40b30f3b7f1
Rajiuddin <skrajiuddin18@gmail.com>
2025-06-05 17:32:31 +0530

    Basic structure for a React e-commerce application done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {Routes,Route} from 'react-router-dom';
+import { ProductPage } from './components/ProductPage';
+import { PageNotFound } from './components/PageNotFound';
+import { CartPage } from './components/CartPage';
+
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Header />
+      <Routes>
+        <Route path="/" element={<ProductPage/>} />
+        <Route path="cart" element={<CartPage/>} />
+        <Route path="*" element={<PageNotFound/>} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
